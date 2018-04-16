1bc543a8c9fcdc31fc801b1e35942e0eb93ce1dd
Patrick Vanderwall <patrick.vanderwall23@gmail.com>
2018-04-15 22:18:10 -0700

    Install django-cors-headers and add it to settings.py

--- banger_music/config/settings/settings.py
+++ banger_music/config/settings/settings.py
@@ -38,9 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'webpack_loader',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
