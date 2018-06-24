1673f2e57e33aaa92596fc49507e941d6a86ca98
Patrick Vanderwall <patrick.vanderwall23@gmail.com>
2018-06-23 17:06:18 -0700

    Install djangorestframework-jwt package and set default authentication settings

--- banger_music/config/settings/settings.py
+++ banger_music/config/settings/settings.py
@@ -150,11 +150,14 @@ WEBPACK_LOADER = {
 }
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+     'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 }
 
 
