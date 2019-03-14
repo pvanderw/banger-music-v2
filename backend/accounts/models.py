4acb53d5583144b3f53f3ab556cce16a2a5d3813
Patrick Vanderwall <patrick.vanderwall23@gmail.com>
2019-03-13 17:58:28 -0700

    Remove default timezone from user profile

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -15,7 +15,7 @@ def jwt_get_secret_key(user_model):
 class UserProfile(UUIDModel):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	bio = models.CharField(max_length=500, blank=True)
-	time_zone = models.CharField(max_length=100, default='America/Los_Angeles')
+	time_zone = models.CharField(max_length=100, blank=True, null=True)
 	jwt_secret = models.UUIDField(default=uuid.uuid4)
 
 	def __str__(self):
