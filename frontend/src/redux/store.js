8bbbd498060193b101ced03ea007fddd52bc143c
Patrick Vanderwall <patrick.vanderwall23@gmail.com>
2021-09-15 17:34:18 -0700

    Delete old redux actions/reducers

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,3 @@
-// import { createBrowserHistory } from 'history';
-// import { routerMiddleware } from 'connected-react-router';
-// import { createStore, compose, applyMiddleware } from 'redux';
-// import createRootReducer from './reducers/rootReducer';
-// import thunk from 'redux-thunk';
-
-
 import { configureStore } from '@reduxjs/toolkit'
 
 import authReducer from '../features/auth/authSlice';
@@ -16,29 +9,3 @@ const store = configureStore({
 });
 
 export default store;
-
-// export const history = createBrowserHistory();
-
-// const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-
-// export default function configureStore(preloadedState) {
-//   const store = createStore(
-//     createRootReducer(history), // root reducer with router state
-//     preloadedState,
-//     composeEnhances(
-//       applyMiddleware(
-//         routerMiddleware(history), // for dispatching history actions
-//         thunk,
-//       ),
-//     ),
-//   );
-
-//   return store
-// }
-
-// const store = createStore(reducer, composeEnhances(
-//     applyMiddleware(thunk)
-// ));
-
-
